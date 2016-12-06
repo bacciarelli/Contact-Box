7d339bb0bdc7e205ab1672df6fc4ee5f6aa4af16
Michal Bacciarelli <michal.bacciarelli@gmail.com>
2016-12-06 22:09:42 +0100

    added mail entity

---
+++ contact_box/src/ContactBoxBundle/Entity/mail.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace ContactBoxBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * mail
+ *
+ * @ORM\Table(name="mail")
+ * @ORM\Entity(repositoryClass="ContactBoxBundle\Repository\mailRepository")
+ */
+class mail
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email_address", type="string", length=255)
+     */
+    private $emailAddress;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     */
+    private $type;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set emailAddress
+     *
+     * @param string $emailAddress
+     * @return mail
+     */
+    public function setEmailAddress($emailAddress)
+    {
+        $this->emailAddress = $emailAddress;
+
+        return $this;
+    }
+
+    /**
+     * Get emailAddress
+     *
+     * @return string 
+     */
+    public function getEmailAddress()
+    {
+        return $this->emailAddress;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     * @return mail
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+}
